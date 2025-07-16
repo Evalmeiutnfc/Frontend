99d968ebf91ac240c4b59277f45906c3c7c40140
majoiefaya <majoiefaya@gmail.com>
2025-07-16 15:12:37 +0200

    Version deployée

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,21 @@ import vue from '@vitejs/plugin-vue'
 import vuetify from 'vite-plugin-vuetify' // 👈 à ajouter
 
 export default defineConfig({
+  base: '/Frontend/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
   plugins: [
     vue(),
     vuetify({ autoImport: true }) // 👈 à ajouter
   ],
+  build: {
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        // Améliorer la gestion des chunks
+        manualChunks: {
+          vendor: ['vue', 'vue-router'],
+          vuetify: ['vuetify']
+        }
+      }
+    }
+  }
 })
